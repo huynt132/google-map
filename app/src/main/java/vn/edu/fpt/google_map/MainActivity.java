78afe0c60d0ac77b6dbf57832fb60b40d0d453d6
huynt <huynthe141627@fpt.edu.vn>
2021-10-06 19:28:05 +0700

    add marker on tap

--- app/src/main/java/vn/edu/fpt/google_map/MainActivity.java
+++ app/src/main/java/vn/edu/fpt/google_map/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -19,6 +20,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -56,10 +58,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         setContentView(R.layout.activity_main);
         imageViewSearch = findViewById(R.id.imageViewSearch);
         inputLocation = findViewById(R.id.inputLocation);
-        //check permission to access current location of user
         checkPermission();
 
-        //check permission granted or not
         if (isPermissionGranted) {
             if (checkGoolePlaServices()) {
                 //show map when permission is granted
@@ -69,6 +69,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             } else {
                 Toast.makeText(this, "Google Playservices Not Available", Toast.LENGTH_SHORT).show();
             }
+        } else {
+            Toast.makeText(this, "Pls grant permission", Toast.LENGTH_SHORT).show();
         }
 
         imageViewSearch.setOnClickListener(new View.OnClickListener() {
@@ -82,32 +84,45 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
                     try {
                         List<Address> listAddress = geocoder.getFromLocationName(location, 1);
+
                         if (listAddress.size() > 0) {
-                            //lấy ra kinh độ và vĩ độ của địa chỉ mà người dùng search của object đầu tiên được tìm thấy trong list Address
+
+                            hideSoftKeyboard(findViewById(android.R.id.content).getRootView());
+
                             LatLng latLng = new LatLng(listAddress.get(0).getLatitude(), listAddress.get(0).getLongitude());
 
-                            //move đến vị trí người dùng search
                             MarkerOptions markerOptions = new MarkerOptions();
-                            markerOptions.title("Search Position");
-                            //pass latLng to market position
+                            markerOptions.title(listAddress.get(0).getAddressLine(0));
                             markerOptions.position(latLng);
                             googleMap.addMarker(markerOptions);
 
-                            //Camera animate to zoom in 15 times to the marked location
-                            movePosition(latLng, googleMap, 5);
+                            movePosition(latLng, googleMap, 12);
 
                             //show the text of searched COUNTRY NAME
                             Toast.makeText(MainActivity.this, "" + listAddress.get(0).getCountryName(), Toast.LENGTH_SHORT).show();
+                        } else {
+                            hideSoftKeyboard(findViewById(android.R.id.content).getRootView());
+                            Toast.makeText(MainActivity.this, "Location not found!!", Toast.LENGTH_SHORT).show();
                         }
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-
                 }
             }
         });
     }
 
+    /*
+     this method use to hide virtual input keyboard
+     */
+    public void hideSoftKeyboard(View view) {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    /*
+     this method use to move the map position by LatLng position
+     */
     private void movePosition(LatLng position, GoogleMap map, int zoomValue) {
         //Camera animate to zoom in 15 times to the marked location
         CameraPosition cameraPosition = new CameraPosition.Builder()
@@ -139,7 +154,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         return false;
     }
 
-
+    /*
+     this method use to check permission to access current location of user
+     */
     private void checkPermission() {
         Dexter.withContext(this).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
             @Override
@@ -161,57 +178,64 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
             @Override
             public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
-
                 //keep continue show permission request
                 permissionToken.continuePermissionRequest();
             }
         }).check();
     }
 
+    /*
+     this method use to set up and initial for map when map is ready
+     */
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-        this.googleMap = googleMap;//ggmap
-//        googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        this.googleMap = googleMap;
 
-        //latLng dùng để lưu vị trí kinh độ của market location
+        //mark on tap
+        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(@NonNull LatLng latLng) {
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(latLng);
+                googleMap.clear();
+                googleMap.addMarker(markerOptions);
+            }
+        });
+
+        //create initial position
         LatLng latLng = new LatLng(21.013550445956177, 105.52711794224213);
         MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.title("My position");
-
-        //pass latLng to market position
+        markerOptions.title("FPT university");
         markerOptions.position(latLng);
         googleMap.addMarker(markerOptions);
-
         movePosition(latLng, googleMap, 15);
 
-        googleMap.getUiSettings().setZoomControlsEnabled(true);//enable zoom in zoom out in icon at the left bottom corner
-//        googleMap.getUiSettings().setCompassEnabled(true);
-        googleMap.getUiSettings().setZoomControlsEnabled(true);//enable double click to zoom
-//        googleMap.getUiSettings().setScrollGesturesEnabled(false);//disable scroll mouse
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+        //googleMap.getUiSettings().setScrollGesturesEnabled(false);//disable scroll mouse
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             return;
         }
         googleMap.setMyLocationEnabled(true);
-
     }
 
+    /*
+     This method use to assign options menu for menu activity
+    */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
 
+    /*
+     This method use to handle when selected event raise
+     */
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
         //change the map type depend on user selected option
         if (item.getItemId() == R.id.noneMap) {
             googleMap.setMapType(GoogleMap.MAP_TYPE_NONE);
